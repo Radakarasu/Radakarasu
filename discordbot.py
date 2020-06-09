e4a7d022aa9afa242ab870450795532774411703
Radakarasu <66629306+Radakarasu@users.noreply.github.com>
2020-06-09 19:19:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,4 +15,24 @@ async def ping(ctx):
 async def やぁ(ctx):
     await ctx.send('よぉ')     
     
+    
+@bot.command()
+async def 死ね(ctx):
+    await ctx.send('は？')     
+    
+    
+@bot.command()
+async def pornhub(ctx):
+    await ctx.send('https://jp.pornhub.com/')  
+    
+    
+@bot.command()
+async def ロリ(ctx):
+    await ctx.send('http://moeimg.net/tag/%E3%83%AD%E3%83%AA')     
+    
+    
+@bot.command()
+async def 手マン(ctx):
+    await ctx.send('https://jp.pornhub.com/video/search?search=%E6%89%8B%E3%83%9E%E3%83%B3')     
+    
 bot.run(token)
