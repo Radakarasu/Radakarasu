9a4504d0db05f095ba2808306f324e2f564841fc
Radakarasu <66629306+Radakarasu@users.noreply.github.com>
2020-06-09 17:58:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,23 +1,10 @@
 from discord.ext import commands
 import os
-import tasks
 import traceback
 
 
 bot = commands.Bot(command_prefix='！')
 token = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = 659232515319529480 #チャンネルID
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
-
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('時間だよ')  
-
-#ループ処理実行
-loop.start()
 
 @bot.command()
 async def ping(ctx):
