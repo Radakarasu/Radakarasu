629ab218d3ff58a59c464d3f091f9d08956bd41c
Radakarasu <66629306+Radakarasu@users.noreply.github.com>
2020-06-09 20:51:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,27 @@
 from discord.ext import commands
 import os
 import traceback
+from discord.ext import tasks
+from datetime import datetime 
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+CHANNEL_ID = 659232515319529480 #チャンネルID
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+        
+        
+    @tasks.loop(seconds=10)
+async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '20:52':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('おはよう')  
 
+#ループ処理実行
+loop.start()    
+        
         
 @bot.command()
 async def ping(ctx):
